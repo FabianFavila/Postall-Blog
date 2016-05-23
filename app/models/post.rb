3cf1a1e268a2d50505cf6ea050f9c104392d9d09
Fabian Favila <solanofavilaf@gmail.com>
2016-05-23 11:23:16 -0500

    Modelo de Post modificado

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+	validates :title, length: {
+    minimum: 5,
+    maximum: 30,
+    message: "Titulo invalido"
+  	}
+
+  	validates :body, length: {
+    minimum: 10,
+    maximum: 50,
+    too_short: "Post muy corto, debe tener al menos %{count} caracteres.",
+    too_long: "Post muy largo, debe tener maximo %{count} caracteres."
+  	}
 end
